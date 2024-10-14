2a8e394238be4a43993c3e0c23ec479c3cff7e44
bs-jenk <jenkinson.sam@gmail.com>
2024-10-14 12:07:56 +0100

    create app.js and server.test.js files and add seeding functionalty with test data

---
+++ __tests__/server.test.js
@@ -0,0 +1,19 @@
+const app = require("../app");
+const request = require("supertest");
+const db = require("../db/connection");
+const {
+    articleData,
+    commentData,
+    topicData,
+    userData,
+} = require("../db/data/test-data");
+const seed = require("../db/seeds/seed");
+
+beforeEach(() => seed({ articleData, commentData, topicData, userData }));
+afterAll(() => db.end());
+
+describe("API endpoints", () => {
+    describe("", () => {
+        it("", () => {});
+    });
+});
