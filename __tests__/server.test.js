f7fb1d0d2270f68d9f73a3ea029f817f1d7328e4
bs-jenk <jenkinson.sam@gmail.com>
2024-10-14 14:40:40 +0100

    add test and code to catch any invalid endpoints

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -12,6 +12,17 @@ const seed = require("../db/seeds/seed");
 beforeEach(() => seed({ articleData, commentData, topicData, userData }));
 afterAll(() => db.end());
 
+describe("* ALL incorrect endpoints", () => {
+    test("404 - sends an error message to the client when a request is made to a path that does not exist", () => {
+        return request(app)
+            .get("/api/notvalid")
+            .expect(404)
+            .then((response) => {
+                expect(response.body.msg).toBe("ERROR: path not found");
+            });
+    });
+});
+
 describe("API endpoints", () => {
     describe("GET: /api/topics ", () => {
         test("200 - sends an array of topics to the client", () => {
